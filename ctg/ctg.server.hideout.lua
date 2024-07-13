adcef51a5ed2db455438dca6454ad656ab777ab0
Ola Gawell <ola.gawell@devoteam.com>
2024-07-13 20:54:12 +0200

    Added first code for hideout [dev-1]

---
+++ ctg/ctg.server.hideout.lua
@@ -0,0 +1,44 @@
+local hideouts
+local hidoutMarker = nil
+
+function setHideouts(spawns)
+    hideouts = spawns
+end
+
+function spawnNewHideout()
+    removeOldHideout()
+
+    local spawnPoint = hideouts[math.random(#hideouts)]
+    local posX, posY, posZ = coordsFromEdl(spawnPoint)
+    local rotX, rotY, rotZ = rotFromEdl(spawnPoint)
+
+    if (hideoutMarker == nil) then
+        hideoutMarker = createMarker(posX, posY, posZ, "arrow", 2.0, 255, 0, 0)
+    end
+	createBlip(posX, posY, posZ, 52)
+end
+
+function showBlip(carrier)
+	destroyElementsByType ("blip")
+	createBlipAttachedTo ( carrier, 0 )
+end
+
+function removeOldHideout()
+	destroyElementsByType ("blip")
+    destroyMarker()
+end
+
+function destroyMarker()
+    if (hideoutMarker ~= nil) then
+        destroyElement(hideoutMarker)
+    end
+    hideoutMarker = nil
+end
+
+function markerHit(markerHit, matchingDimension)
+    if markerHit == hideoutMarker then
+        removeOldHideout()
+        return
+    end
+end
+addEventHandler("onPlayerMarkerHit", getRootElement(), markerHit)
