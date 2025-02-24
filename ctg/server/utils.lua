d71dfbdaf0899355030273896fde69789d48f22b
Ola Gawell <ola.gawell@devoteam.com>
2025-02-24 11:42:34 +0100

    Better distance function

--- ctg/server/utils.lua
+++ ctg/server/utils.lua
@@ -9,20 +9,31 @@ function chooseRandomCloseTo(edls, position, wantedRadius)
         table.insert(edlsWithDistances, {edl = edl, distance = distanceFromRadius, x, y, z})
     end
 
+    local longestDistance = 0
+    for i, v in ipairs(edlsWithDistances) do
+        if v.distance > longestDistance then
+            longestDistance = v.distance
+        end
+    end
+
+    for i, v in ipairs(edlsWithDistances) do
+        v.quota = longestDistance + 200 - v.distance
+    end
+
     -- sort the table by longest distance first
-    table.sort(edlsWithDistances, function(a, b) return a.distance > b.distance end)
+    table.sort(edlsWithDistances, function(a, b) return a.quota > b.quota end)
 
     -- pick a random element based on quota where the quota is the index in the table
     local totalQuota = 0
-    for k, _ in ipairs(edlsWithDistances) do
-        totalQuota = totalQuota + k
+    for _, v in ipairs(edlsWithDistances) do
+        totalQuota = totalQuota + v.quota
     end
 
     -- get a random number between 0 and totalQuota
     local random = math.random() * totalQuota
     -- iterate over the table and subtract the quota of each element from the random number
     for k, v in pairs(edlsWithDistances) do
-        random = random - k
+        random = random - v.quota
         -- if the random number is less than 0, return the edl
         if random < 0 then
             return v.edl
@@ -30,6 +41,38 @@ function chooseRandomCloseTo(edls, position, wantedRadius)
     end
 
     return nil
+end
+
+function positionCloseTo(edls, firstPosition, firstRadius, secondPosition, secondRadius, secondWeight)
+    if not edls or #edls == 0 then
+        return nil
+    end
+
+    local edlsWithDistances = {}
+    for i, edl in ipairs(edls) do
+        local x, y, z = coordsFromEdl(edl)
+        local distance = getDistanceBetweenPoints3D(x, y, z, position.x, position.y, position.z)
+        local distanceFromRadius = math.abs(distance - wantedRadius)
+
+        local secondDistance = -1
+        if secondPosition then
+            local distance2 = getDistanceBetweenPoints3D(x, y, z, secondPosition.x, secondPosition.y, secondPosition.z)
+            secondDistance = math.abs(distance2 - secondRadius)
+        end
+
+        table.insert(edlsWithDistances, {edl = edl, distance = distanceFromRadius, x, y, z, secondDistance})
+    end
+
+    if secondWeight then
+        table.sort(edlsWithDistances, function(a, b) 
+            return (a.distance + a.secondDistance * secondWeight) > (b.distance + b.secondDistance * secondWeight)
+        end)
+    else
+        table.sort(edlsWithDistances, function(a, b) 
+            return a.distance > b.distance 
+        end)
+    end
+    return edlsWithDistances[1].edl
 end
 
 function meanPositionOfPlayers()
