323fd438ba2bbc2ae77b1f6386085dc25b8a01cb
Ola Gawell <ola.gawell@devoteam.com>
2025-02-25 22:32:34 +0100

    More data in power box

--- ctg/client/powerconfig.window.lua
+++ ctg/client/powerconfig.window.lua
@@ -1,34 +1,101 @@
 local powerwindow = nil
 local scrollpane = nil
 
-function createPowerBox()
+local dummyData = {
+    active = {
+        { key = "nitro", bindKey = "lctrl" },
+        { key = "teleport", bindKey = "X" },
+        --{ key = "busses", bindKey = "C" },
+        --{ key = "waterLevel", bindKey = "R" },
+        { key = "canon", bindKey = "C" },
+        --{ key = "plane", bindKey = "P" },
+        --{ key = "cinematic", bindKey = "N" },
+    },
+    wanted = {},
+    owned = {
+        "nitro",
+        "teleport",
+        "canon",
+        "superCar",
+    },
+    all = {
+        "nitro",
+        "teleport",
+        "busses",
+        "waterLevel",
+        "canon",
+        "plane",
+        "cinematic",
+        "superCar",
+    }
+}
+
+function getPowerUp(key)
+    return {
+        key = key,
+        name = "The "..key.." power",
+        desc = "This is a description of "..key..". It can be very long",
+        cooldown = 20,
+        duration = 10,
+        charges = -1,
+        initCooldown = 10,
+        allowedGoldCarrier = false,
+    }
+end
+
+function 
+
+function isOwned(key)
+    for k, v in ipairs(dummyData.owned) do
+        if v == key then
+            return true
+        end
+    end
+    return false
+end
+
+function createPowerBox(powerUp, isOwned, row, col)
     powerbox = guiCreateButton(0.01, 0.10, 0.16, 0.24, "", true, scrollpane)
 
-    powerTitle = guiCreateLabel(0.06, 0.05, 0.87, 0.08, "Power Name", true, powerbox)
+    powerTitle = guiCreateLabel(0.06, 0.05, 0.87, 0.08, powerUp.name, true, powerbox)
     guiSetFont(powerTitle, "clear-normal")
     guiLabelSetHorizontalAlign(powerTitle, "center", false)
-    unlockButton = guiCreateButton(0.71, 0.86, 0.23, 0.10, "Unlock", true, powerbox)
+
     durationTitle = guiCreateLabel(0.06, 0.13, 0.87, 0.05, "Duration: ", true, powerbox)
-    guiSetFont(durationTitle, "default-bold-small")
-
-    durationValue = guiCreateLabel(0.24, 0.07, 0.24, 0.93, "10s", true, durationTitle)
-
-    GUIEditor.label[1] = guiCreateLabel(649, -58, 255, 15, "Duration: ", false, powerbox)
-    guiSetFont(GUIEditor.label[1], "default-bold-small")
     cooldownTitle = guiCreateLabel(0.06, 0.18, 0.87, 0.05, "Cooldown: ", true, powerbox)
-    guiSetFont(cooldownTitle, "default-bold-small")
-
-    cooldownValue = guiCreateLabel(0.25, 0.00, 0.16, 1.00, "20s", true, cooldownTitle)
-
-    bindXButton = guiCreateButton(0.05, 0.86, 0.28, 0.10, "Bind X", true, powerbox)
     usedAsGoldCarrierTitle = guiCreateLabel(0.42, 0.13, 0.30, 0.16, "Can be used when carrying gold:", true, powerbox)
+    guiSetFont(durationTitle, "default-bold-small")
+    guiSetFont(cooldownTitle, "default-bold-small")
     guiSetFont(usedAsGoldCarrierTitle, "default-bold-small")
     guiLabelSetHorizontalAlign(usedAsGoldCarrierTitle, "right", true)
-    usedAsGoldCarrierValue = guiCreateLabel(0.76, 0.13, 0.18, 0.16, "Yes", true, powerbox)
 
-    description = guiCreateLabel(0.01, 0.09, 0.14, 0.14, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", true, powerbox)
+    if not isOwned then
+        unlockButton = guiCreateButton(0.71, 0.86, 0.23, 0.10, "Unlock", true, powerbox)
+    else
+        bindXButton = guiCreateButton(0.05, 0.86, 0.28, 0.10, "Bind X", true, powerbox)
+        bindCButton = guiCreateButton(0.06, 0.24, 0.05, 0.03, "Bind C", true, powerbox)
+    end
+
+    durationValue = guiCreateLabel(0.24, 0.07, 0.24, 0.93, powerUp.duration.."s", true, durationTitle)
+    cooldownValue = guiCreateLabel(0.25, 0.00, 0.16, 1.00, powerUp.cooldown.."s", true, cooldownTitle)
+    local canBeYsedAsGoldCarrier = powerUp.allowedGoldCarrier and "Yes" or "No"
+    usedAsGoldCarrierValue = guiCreateLabel(0.76, 0.13, 0.18, 0.16, canBeYsedAsGoldCarrier, true, powerbox)
+    description = guiCreateLabel(0.01, 0.09, 0.14, 0.14, powerUp.name, true, powerbox)
+
     guiLabelSetHorizontalAlign(description, "left", true)
-    bindCButton = guiCreateButton(0.06, 0.24, 0.05, 0.03, "Bind C", true, powerbox)
+
+    return {
+        powerUp = powerUp,
+        box = powerbox,
+        title = powerTitle,
+        duration = durationValue,
+        cooldown = cooldownValue,
+        bindX = bindXButton,
+        bindC = bindCButton,
+        unlock = unlockButton,
+        usedAsGoldCarrier = usedAsGoldCarrierValue,
+        description = description
+    }
 end
 
 addEventHandler("onClientResourceStart", resourceRoot,
@@ -47,7 +114,18 @@ addEventHandler("onClientResourceStart", resourceRoot,
 
         doneButton = guiCreateButton(0.93, 0.02, 0.06, 0.05, "Done", true, powerwindow)
 
-        createPowerBox()
+        local row = 1
+        local col = 1
+        for k, v in ipairs(dummyData.all) do
+            local powerUp = getPowerUp(v)
+            local isOwned = isOwned(v)
+            createPowerBox(powerUp, isOwned, row, col)
+            col = col + 1
+            if col > 5 then
+                col = 1
+                row = row + 1
+            end
+        end
         
     end
 )
