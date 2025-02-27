02846e92638f8408d0396f6b39fc37a056df8310
Gaffel <gaffel@grapen.se>
2025-02-27 12:10:34 +0100

    Better power boxes

--- ctg/client/power.gui.lua
+++ ctg/client/power.gui.lua
@@ -1,5 +1,5 @@
 local powerBoxes = {}
-local xDiff = 0.1
+local xDiff = 0.12
 
 addEvent("boosterCooldownTick", true)
 addEvent("boosterDurationTick", true)
@@ -10,23 +10,30 @@ addEvent("powerupSetDurationClient", true)
 
 function createPowerUpBox(index)
     local posX = 0.7 - xDiff * index
-    local powerwindow = guiCreateWindow(posX, 0.92, 0.08, 0.06, "Power Up", true)
+    local powerwindow = guiCreateWindow(posX, 0.92, 0.1, 0.06, "Power Up", true)
     guiWindowSetSizable(powerwindow, false)
     guiSetAlpha(powerwindow, 1.00)
     guiSetProperty(powerwindow, "CaptionColour", "FEFFFFFF")
 
-    local powercooldown = guiCreateProgressBar(54, 45, 85, 12, false, powerwindow)
+    local powercooldown = guiCreateProgressBar(0, 0.3, 1, 1, true, powerwindow)
     guiSetAlpha(powercooldown, 0.88)
     guiProgressBarSetProgress(powercooldown, 42)
-    local powerbutton = guiCreateButton(10, 21, 36, 36, "lctrl", false, powerwindow)
+    local powerbutton = guiCreateButton(0.3, 0.3, 0.4, 0.9, "lctrl", true, powerwindow)
     guiSetFont(powerbutton, "clear-normal")
     guiSetProperty(powerbutton, "NormalTextColour", "FEFFFFFF")
+    
     local charge1 = guiCreateRadioButton(56, 26, 14, 15, "", false, powerwindow)
     local charge2 = guiCreateRadioButton(74, 26, 14, 15, "", false, powerwindow)
     local charge3 = guiCreateRadioButton(92, 26, 14, 15, "", false, powerwindow)
     local charge4 = guiCreateRadioButton(110, 26, 14, 15, "", false, powerwindow)
     local charge5 = guiCreateRadioButton(128, 26, 14, 15, "", false, powerwindow)
     local status = guiCreateLabel(53, 26, 88, 19, "Status update", false, powerwindow)
+
+    guiSetVisible(charge1, false)
+    guiSetVisible(charge2, false)
+    guiSetVisible(charge3, false)
+    guiSetVisible(charge4, false)
+    guiSetVisible(charge5, false)
     guiSetVisible(status, false)
     --guiRadioButtonSetSelected(charge3, true)
     return {
@@ -69,8 +76,12 @@ function tickPowerUpCooldown(timeLeft, totalTime, index, name, key, enabled)
 
 	local progress = 100 * (totalTime - timeLeft)/totalTime
 	if ( progress < 99.5 ) then
+        guiSetVisible(powerBox.button, false)
+        guiSetVisible(powerBox.progress, true)
 		guiSetAlpha ( powerBox.window,0.5 )
 	else 
+        guiSetVisible(powerBox.button, true)
+        guiSetVisible(powerBox.progress, false)
         guiSetAlpha ( powerBox.button, 1 )
 		guiSetAlpha ( powerBox.window, 1 )
 	end
@@ -85,6 +96,8 @@ function tickPowerUpDuration(timeLeft, totalTime, index, name, key, enabled)
 	local progress = 100 - (100 * (totalTime - timeLeft)/totalTime)
 	-- guiLabelSetColor ( boosterLabel, 77, 77, 77 )
     guiSetAlpha ( powerBox.button, 0.5 )
+    guiSetVisible(powerBox.button, false)
+    guiSetVisible(powerBox.progress, true)
 	guiProgressBarSetProgress(powerBox.progress, progress)
 end
 addEventHandler("boosterDurationTick", getRootElement(), tickPowerUpDuration)
