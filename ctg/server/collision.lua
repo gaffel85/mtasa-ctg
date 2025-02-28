aa0c342d1b187fa576cffdfadcd840e41b5e19e0
Gaffel <gaffel@grapen.se>
2025-02-28 22:29:39 +0100

    Fixed shield power up

--- ctg/server/collision.lua
+++ ctg/server/collision.lua
@@ -6,7 +6,7 @@ function isShielded(player)
 end
 
 function addShieldedPlayer(player)
-    outputChatBox('--Adding shield to player '..inspect(otherPlayer))
+    --outputChatBox('--Adding shield to player '..inspect(otherPlayer))
     shieldedPlayers[player] = true
 end
 
@@ -20,11 +20,13 @@ function removeShieldedPlayer(player)
 end
 
 function collisisionWithPlayer(otherPlayer, damage)
-    outputChatBox('Collision with player '..inspect(otherPlayer).. ' 1 '..inspect(client).. ' 2 '..inspect(source).. ' 3 '..inspect(sourceRoot))
+    --outputChatBox('Collision with player '..inspect(otherPlayer).. ' 1 '..inspect(client).. ' 2 '..inspect(source).. ' 3 '..inspect(sourceRoot))
     local goldCarrier = getGoldCarrier()
     local notGoldCarrier = nil
-    if otherPlayer ~= goldCarrier then
-        notGoldCarrier = source
+    if otherPlayer == goldCarrier then
+        notGoldCarrier = client
+    elseif client == goldCarrier then
+        notGoldCarrier = otherPlayer
     end
 
     if not notGoldCarrier then
@@ -32,18 +34,18 @@ function collisisionWithPlayer(otherPlayer, damage)
     end
 
     if isShielded(goldCarrier) then
-        outputChatBox('Gold carrier was shielded player '..inspect(goldCarrier))
+        --outputChatBox('Gold carrier was shielded player '..inspect(goldCarrier))
         local oldShieldTimer = shieldTimers[goldCarrier]
         if not oldShieldTimer then
             shieldTimers[goldCarrier] = setTimer(function()
-                outputChatBox('Remove shield by timer  for gold carrier '..inspect(goldCarrier))
+                --outputChatBox('Remove shield by timer  for gold carrier '..inspect(goldCarrier))
                 removeShieldedPlayer(goldCarrier)
                 triggerClientEvent(getRootElement(), "onShieldRemovedFromServer", resourceRoot, goldCarrier)
             end, 1000, 1)
         end
     else 
-        outputChatBox('No shield '..inspect(otherPlayer))
-        changeGoldCarrier(otherPlayer)
+        --outputChatBox('No shield '..inspect(otherPlayer))
+        changeGoldCarrier(notGoldCarrier)
     end
 end
 addEvent("onCollisionWithPlayer", true)
