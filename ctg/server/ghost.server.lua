611e058dc23ffd45d949ec3afff4c13edd60cd98
Gaffel <gaffel@grapen.se>
2025-03-17 12:16:07 +0100

    wip

--- ctg/server/ghost.server.lua
+++ ctg/server/ghost.server.lua
@@ -2,14 +2,14 @@ function setPlayerGhost(player)
     return setElementData(player, "isGhost", true)
 end
 
-function makePlayerGhost(plauer, duration, safeCheck)
+function makePlayerGhost(player, duration, safeCheck)
     setPlayerGhost(player)
     triggerClientEvent("makeGhostFromServer", getRootElement())
 end
 
-registerBindFunctions(function()
-    bindKey(player, "g", "down", makePlayerGhost)
-end, function()
-    unbindKey(player, "g", "down", makePlayerGhost)
+registerBindFunctions(function(player)
+    bindKey(player, "g", "down", makePlayerGhost, player, 30, true)
+end, function(player)
+    unbindKey(player, "g", "down", makePlayerGhost, player)
 end)
 
