6e8926ef96acf5e165d41e6a2d8c4457d65a7665
Ola Gawell <ola.gawell@devoteam.com>
2025-03-18 12:18:26 +0100

    Only save z rotation

--- ctg/server/location.db.lua
+++ ctg/server/location.db.lua
@@ -1,12 +1,15 @@
 local locations = {}
 local minDistance = 5
+local minSpeedForRotation = 50
 local filePath = "locations.json"
 
-function hasLocationCloseTo(x, y, z)
+function hasLocationCloseToForPlayer(x, y, z, player)
     -- check if there is a location close to x, y, z
     for i, location in ipairs(locations) do
-        if getDistanceBetweenPoints3D(x, y, z, location.x, location.y, location.z) < minDistance then
-            return true
+        if player == nil or location.player == player then
+            if getDistanceBetweenPoints3D(x, y, z, location.x, location.y, location.z) < minDistance then
+                return true
+            end
         end
     end
     return false
@@ -24,7 +27,7 @@ function saveLocationForPlayer(player)
     end
 
     local x, y, z = getElementPosition(vehicle)
-    if hasLocationCloseTo(x, y, z) then
+    if hasLocationCloseToForPlayer(x, y, z, player) then
         return
     end
 
@@ -43,7 +46,8 @@ function saveLocationForPlayer(player)
         vz = vz,
         avx = avx,
         avy = avy,
-        avz = avz
+        avz = avz,
+        player = player
     })
     plotPosition(x, y, z)
 end
@@ -72,20 +76,35 @@ function readLocationsFromJsonFile()
                 x = locationAsArray[1],
                 y = locationAsArray[2],
                 z = locationAsArray[3],
-                rx = locationAsArray[4],
-                ry = locationAsArray[5],
-                rz = locationAsArray[6],
-                vx = locationAsArray[7],
-                vy = locationAsArray[8],
-                vz = locationAsArray[9],
-                avx = locationAsArray[10],
-                avy = locationAsArray[11],
-                avz = locationAsArray[12]
+                rz = locationAsArray[4] or 0,
+                ry = 0,
+                rx = 0,
+                vx = 0,
+                vy = 0,
+                vz = 0,
+                avx = 0,
+                avy = 0,
+                avz = 0
             }
             table.insert(locations, location)
             plotPosition(location.x, location.y, location.z)
         end
     end
+end
+
+function calculateZRotation(vx, vy)
+    -- calculate the rotation around Z-axis from velocity vector
+    local speed = math.sqrt(vx*vx + vy*vy)
+    local speedInKmh = speed * 180
+    if speedInKmh < minSpeedForRotation then
+        return nil
+    end
+
+    local angle = math.deg(math.acos(vx/speed))
+    if vy < 0 then
+        angle = 360 - angle
+    end
+    return angle
 end
 
 function saveWholeFileAsJson()
@@ -101,15 +120,7 @@ function saveWholeFileAsJson()
             location.x,
             location.y,
             location.z,
-            location.rx,
-            location.ry,
-            location.rz,
-            location.vx,
-            location.vy,
-            location.vz,
-            location.avx,
-            location.avy,
-            location.avz
+            calculateZRotation(location.vx, location.vy),
         })
     end
 
