bc2031c867e87135b17ba85fd6ce8b770b2d21a4
Ola Gawell <ola.gawell@devoteam.com>
2025-03-19 13:23:28 +0100

    Only plot every 30:e location

--- ctg/server/location.db.lua
+++ ctg/server/location.db.lua
@@ -1,91 +1,43 @@
 local locations = {}
-local locationsFromClient = {}
-local minDistance = 5
-local minSpeedForRotation = 50
+local pointsToPlot = {}
+local blips = {}
+local plotDistance = 30
 local filePath = "locations.json"
-
---[[
-
-function hasLocationCloseToForPlayer(x, y, z, player)
-    -- check if there is a location close to x, y, z
-    for i, location in ipairs(locations) do
-        if player == nil or location.player == player then
-            if getDistanceBetweenPoints3D(x, y, z, location.x, location.y, location.z) < minDistance then
-                return true
-            end
-        end
-    end
-    return false
-end
-
-function saveLocationForPlayer(player)
-    -- save location, rotation, velocity and angular velocity for player
-    local vehicle = getPedOccupiedVehicle(player)
-    if not vehicle then
-        return
-    end
-
-    if  not isVehicleOnGround(vehicle) then
-        return
-    end
-
-    local x, y, z = getElementPosition(vehicle)
-    if hasLocationCloseToForPlayer(x, y, z, player) then
-        return
-    end
-
-    local rx, ry, rz = getElementRotation(vehicle)
-    local vx, vy, vz = getElementVelocity(vehicle)
-    local avx, avy, avz = getElementAngularVelocity(vehicle)
-    table.insert(locations, {
-        x = x,
-        y = y,
-        z = z,
-        rx = rx,
-        ry = ry,
-        rz = rz,
-        vx = vx,
-        vy = vy,
-        vz = vz,
-        avx = avx,
-        avy = avy,
-        avz = avz,
-        player = player
-    })
-    plotPosition(x, y, z)
-end
-
-function calculateZRotation(vx, vy)
-    -- calculate the rotation around Z-axis from velocity vector
-    local speed = math.sqrt(vx*vx + vy*vy)
-    local speedInKmh = speed * 180
-    if speedInKmh < minSpeedForRotation then
-        return nil
-    end
-
-    local angle = math.deg(math.acos(vx/speed)) - 90
-    if vy < 0 then
-        angle = 360 - angle
-    end
-    return angle
-end
-
-function saveLocationsForAllPlayers()
-    for i, player in ipairs(getElementsByType("player")) do
-        saveLocationForPlayer(player)
-    end
-end
-]]--
 
 function plotPosition(x, y, z)
     -- plot a position in the world
-    createBlip(x, y, z, 0, 2, 0, 255, 255, 255, 0)
+    local blip = createBlip(x, y, z, 0, 2, 0, 255, 255, 255, 0)
+    table.insert(blips, blip)
 end
 
 function plotAllPositions()
+    for i, blip in ipairs(blips) do
+        destroyElement(blip)
+    end
     -- plot all positions in the world
     for i, location in ipairs(locations) do
         plotPosition(location.x, location.y, location.z)
+    end
+end
+
+function addPlotPoint(newLocation)
+    local x = newLocation.x
+    local y = newLocation.y
+    local z = newLocation.z
+    local found = false
+    for j, point in ipairs(pointsToPlot) do
+        if getDistanceBetweenPoints3D(x, y, z, point.x, point.y, point.z) < plotDistance then
+            found = true
+        end
+    end
+    if not found then
+        table.insert(pointsToPlot, newLocation)
+    end
+end
+
+function findPointsToPlot(newPoints)
+    for i, newLocation in ipairs(newPoints) do
+        addPlotPoint(newLocation)
     end
 end
 
@@ -107,8 +59,9 @@ function readLocationsFromJsonFile()
                 speedMet = locationAsArray[7] or false,
             }
             table.insert(locations, location)
-            plotPosition(location.x, location.y, location.z)
+            addPlotPoint(location)
         end
+        plotAllPositions()
     end
 end
 
@@ -154,62 +107,6 @@ function getPosAndRot()
     return location.x, location.y, location.z, location.rx, location.ry, location.rz
 end
 
---[[
-function appendToFile()
-    local file = nil
-    if fileExists(filePath) then
-        file = fileOpen(filePath)
-    else
-        file = fileCreate(filePath)
-        fileWrite(file, "locations = {\n")
-    end
-
-    if file then                                  -- check if it was successfully opened
-        fileSetPos( file, fileGetSize( file ) )   -- move position to the end of the file
-        for i, location in ipairs(locations) do
-            if not location.write then
-                
-                -- save data in indexed table as array
-                local dataAsArray = {
-                    location.x,
-                    location.y,
-                    location.z,
-                    location.rx,
-                    location.ry,
-                    location.rz,
-                    location.vx,
-                    location.vy,
-                    location.vz,
-                    location.avx,
-                    location.avy,
-                    location.avz
-                }
-                fileWrite(file, "    "..inspect(dataAsArray)..",\n")
-                location.write = true
-            end
-        end
-        fileFlush(file)                           -- Flush the appended data into the file.
-        fileClose(file)                           -- close the file once we're done with it
-    end
-end
-
-function closeFile()
-    local file = nil
-    if fileExists(filePath) then
-        file = fileOpen(filePath)
-    else
-        return
-    end
-
-    if file then                                  -- check if it was successfully opened
-        fileSetPos( file, fileGetSize( file ) )   -- move position to the end of the file
-        fileWrite(file, "}\n")
-        fileFlush(file)                           -- Flush the appended data into the file.
-        fileClose(file)                           -- close the file once we're done with it
-    end
-end
-]]--
-
 addEvent("locationFromClient", true)
 addEventHandler("locationFromClient", resourceRoot,
     function(newLocations)
@@ -225,9 +122,10 @@ addEventHandler("locationFromClient", resourceRoot,
                 speedMet = locationAsArray[7] or false,
             }
             table.insert(locations, location)
-            plotPosition(location.x, location.y, location.z)
-            createMarker(location.x, location.y, location.z, "corona")
+            addPlotPoint(location.x, location.y, location.z)
+            --createMarker(location.x, location.y, location.z, "corona")
         end
+        plotAllPositions()
         --saveLocationForPlayer(client)
     end
 )
