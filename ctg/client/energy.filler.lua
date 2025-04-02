ce18f53aba01828727196be41139e63f2b7aaf73
Hidden identity <noname@proton.me>
2025-04-02 08:13:32 +0200

    Prepare for more client side powers

--- ctg/client/energy.filler.lua
+++ ctg/client/energy.filler.lua
@@ -1,15 +1,22 @@
+DGS = exports.dgs --shorten the export function prefix
+
 local energyBar = nil
 local RESOURCES_KEY = "RESOURCES_KEY"
 local energyResourceKey = "energy"
-local currentAmount = 0
 local timerDiff = 50
 local fillRate = 10
-local sendToServerDiff = 10
-local lastSendToServer = 0
-local isBurning = false
-local burningRate = 0
 
-DGS = exports.dgs --shorten the export function prefix
+local energyState = {
+    key = energyResourceKey,
+    currentAmount = 0,
+    lastSendToServer = 0,
+    isBurning = false,
+    burningRate = 0
+}
+
+function getClientState(resouceKey)
+    return energyState
+end
 
 function createEnergyBar()
     energyBar = guiCreateProgressBar( 0.8, 0.5, 0.1, 0.04, true, nil ) --create the gui-progressbar
@@ -29,7 +36,7 @@ function setEnergyBarProgress(percentage)
     DGS:dgsProgressBarSetProgress(dsgEnergyBar, percentage)
 end
 
-function getEnergyResource()
+function getResourceData(resourceKey)
     local resources = getElementData(resourceRoot, RESOURCES_KEY)
     if not resources then
         outputConsole("Could not get energy resources")
@@ -37,7 +44,7 @@ function getEnergyResource()
     end
 
     for i, resource in ipairs(resources) do
-        if resource.key == energyResourceKey then
+        if resource.key == resourceKey then
             return resource
         end
     end
@@ -46,87 +53,99 @@ function getEnergyResource()
     return nil
 end
 
-function updateServerWithLatestValues()
-    local resource = getEnergyResource()
+function updateServerWithLatestValues(clientResourceState)
+    local resource = getResourceData(clientResourceState.key)
     if not resource then
         return
     end
-    local secondsUntilEnd = (resource.capacity - currentAmount) / fillRate
-    if isBurning then
-        secondsUntilEnd = currentAmount / burningRate
+
+    local secondsUntilEnd = (resource.capacity - clientResourceState.currentAmount) / fillRate
+    if clientResourceState.isBurning then
+        secondsUntilEnd = clientResourceState.currentAmount / clientResourceState.burningRate
     end
     
-    triggerServerEvent("energyAmountChangedFromClient", resourceRoot, energyResourceKey, currentAmount, secondsUntilEnd, isBurning, burningRate, fillRate)
+    triggerServerEvent("energyAmountChangedFromClient", resourceRoot, clientResourceState.key, clientResourceState.currentAmount, secondsUntilEnd, clientResourceState.isBurning, clientResourceState.burningRate, fillRate)
 end
 
-function shouldUpdateServer(resource)
-    if currentAmount == 0 and lastSendToServer > 0 then
+function shouldUpdateServer(resource, clientResourceState)
+    if clientResourceState.currentAmount == 0 and clientResourceState.lastSendToServer > 0 then
         return true
     end
-    if currentAmount == resource.capacity and lastSendToServer < resource.capacity then
+    if clientResourceState.currentAmount == resource.capacity and clientResourceState.lastSendToServer < resource.capacity then
         return true
     end
 
     for i, power in ipairs(findPowersWithResource(resource.key)) do
-        if lastSendToServer < power.minResourceAmount and currentAmount >= power.minResourceAmount then
+        if clientResourceState.lastSendToServer < power.minResourceAmount and clientResourceState.currentAmount >= power.minResourceAmount then
             return true
         end
-        if lastSendToServer >= power.minResourceAmount and currentAmount < power.minResourceAmount then
+        if clientResourceState.lastSendToServer >= power.minResourceAmount and clientResourceState.currentAmount < power.minResourceAmount then
             return true
         end
     end
 
-    return math.abs(currentAmount - lastSendToServer) >= sendToServerDiff
+    return false
 end
 
 function fillBarPeriodically()
-    local resource = getEnergyResource()
+    local resource = getResourceData(energyResourceKey)
     if not resource then
         return
     end
 
-    if isBurning then
-        currentAmount = currentAmount - (burningRate * timerDiff / 1000)
-        if currentAmount < 0 then
-            currentAmount = 0
+    if energyState.isBurning then
+        energyState.currentAmount = energyState.currentAmount - (energyState.burningRate * timerDiff / 1000)
+        if energyState.currentAmount < 0 then
+            energyState.currentAmount = 0
         end
     else
-        currentAmount = currentAmount + (fillRate * timerDiff / 1000)
-        if currentAmount > resource.capacity then
-            currentAmount = resource.capacity
+        energyState.currentAmount = energyState.currentAmount + (fillRate * timerDiff / 1000)
+        if energyState.currentAmount > resource.capacity then
+            energyState.currentAmount = resource.capacity
         end
     end
 
-    if (shouldUpdateServer(resource)) then
-        lastSendToServer = currentAmount
-        updateServerWithLatestValues()
+    if (shouldUpdateServer(resource, energyState)) then
+        energyState.lastSendToServer = energyState.currentAmount
+        updateServerWithLatestValues(energyState)
     end
 
-    local percentage = 100 * currentAmount / resource.capacity
+    local percentage = 100 * energyState.currentAmount / resource.capacity
     setEnergyBarProgress(percentage)
 end
 
 addEvent("resourceInUseFromServer", true) -- (resourceKey, burnRate)
 addEventHandler("resourceInUseFromServer", getRootElement(), function(resourceKey, burnRate, minBurn)
     outputConsole("Burning "..resourceKey.." with rate "..burnRate)
+
+    local clientResourceState = getClientState(resourceKey)
+    if not clientResourceState then
+        return
+    end
     
     if burnRate == 0 then
         if minBurn then
-            currentAmount = math.max(currentAmount - minBurn, 0)
+            clientResourceState.currentAmount = math.max(clientResourceState.currentAmount - minBurn, 0)
         end
     else
-        burningRate = burnRate
-        isBurning = true
+        clientResourceState.burningRate = burnRate
+        clientResourceState.isBurning = true
     end
-    updateServerWithLatestValues()
+    updateServerWithLatestValues(clientResourceState)
 end)
 
 addEvent("resourceNotInUseFromServer", true) -- (resourceKey, totalAmount)
 addEventHandler("resourceNotInUseFromServer", getRootElement(), function(resourceKey, totalAmount)
     outputConsole("Not burning "..resourceKey)
-    isBurning = false
-    burningRate = 0
-    updateServerWithLatestValues()
+
+    local clientResourceState = getClientState(resourceKey)
+    if not clientResourceState then
+        return
+    end
+
+    clientResourceState.isBurning = false
+    clientResourceState.burningRate = 0
+    updateServerWithLatestValues(clientResourceState)
 end)
 
 setEnergyBarProgress(0)
